6930d46e48ec0b8c1655b42433f3c0a66e54cd53
amaury <amaury.civier@gmail.com>
2019-02-10 17:19:38 +0100

    Refactor tsconfig

--- src/renderer/typings.d.ts
+++ src/renderer/typings.d.ts
@@ -1,10 +1,15 @@
 /* SystemJS module definition */
+import { System } from 'systemjs';
+
 declare var nodeModule: NodeModule;
+
 interface NodeModule {
   id: string;
 }
-declare const SystemJS;
+
+declare const SystemJS: System;
 declare var window: Window;
+
 interface Window {
   process: any;
   require: any;
